c7519e6c7b8d4786b67c55a8cadc2a8772de691b
sam <samgathon@hotmail.com>
2019-09-13 12:17:23 -0500

    add migrations

--- fantasybasketball/main_app/models.py
+++ fantasybasketball/main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  user_name = models.CharField(max_length=100)
+  Team = models.CharField(max_length=100)
+  point = models.TextField(max_length=250)
+  rank = models.IntegerField()
