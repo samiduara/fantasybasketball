6e8a564124031b6a2efe29e954105afc1df1a039
[Armando Samperio] <armando.samperio@mavs.uta.edu>
2019-09-13 14:12:21 -0500

    add dashboard function,signup function

--- fantasybasketball/main_app/views.py
+++ fantasybasketball/main_app/views.py
@@ -9,3 +9,19 @@ from . models import Profile
 
 def home(request):
     return render(request, 'home.html')
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def signup(request):
+    error_message=''
+    if request.method == 'POST'
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            error_message = 'Invalid sign up-Try Again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message':error_message}
+    return render(request, 'registration/signup.html', context)
