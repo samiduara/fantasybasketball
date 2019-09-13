75eb2da8e31a7e508c3bc1c716e59d06e21761a6
[Armando Samperio] <armando.samperio@mavs.uta.edu>
2019-09-13 14:06:34 -0500

    imported login,Usercreation,login_required,loginMixin

--- fantasybasketball/main_app/views.py
+++ fantasybasketball/main_app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from . models import Profile
 # Create your views here.
 
