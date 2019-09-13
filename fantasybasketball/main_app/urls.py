89d762c07c4cfd609b37ddf15171b2d8db6c8592
[Armando Samperio] <armando.samperio@mavs.uta.edu>
2019-09-13 14:01:47 -0500

    add dashboard/accounts urls

--- fantasybasketball/main_app/urls.py
+++ fantasybasketball/main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('', views.dashboard, name='dashboard'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
