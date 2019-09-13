326ae55ec148fa88b762f289a45114a7d2a0fede
Cole Holmes <mitchellcholmes@gmail.com>
2019-09-13 12:48:40 -0500

    adds player model

--- fantasybasketball/main_app/models.py
+++ fantasybasketball/main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+  api_id=models.IntegerField()
+  first_name=models.CharField(max_length=50)
+  last_name=models.CharField(max_length=50)
