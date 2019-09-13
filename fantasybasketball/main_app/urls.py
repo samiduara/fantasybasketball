4e721b5080d86126cbde61d0564a72ff764b157e
[Armando Samperio] <armando.samperio@mavs.uta.edu>
2019-09-13 14:02:39 -0500

    fix path name dashboard

--- fantasybasketball/main_app/urls.py
+++ fantasybasketball/main_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('', views.dashboard, name='dashboard'),
+  path('dashboard/', views.dashboard, name='dashboard'),
   path('accounts/signup/', views.signup, name='signup'),
 ]
